212247b9378970886bffe23442ab93d62bf75ea9
Xavier Garabito <mauricioxavier.garabitoramirez@gmail.com>
2020-06-02 23:49:48 -0500

    added get_range and get_func to the function module

--- function.py
+++ function.py
@@ -1,9 +1,7 @@
-t = 1
-l = 1
-c = 1
-ratio = .1
+import math
 
-fun = lambda x: e**(-1*t*x/(2*l))*math.cos(t*math.sqrt(1/(l*c)-(x/(2*l))**2)) - ratio
+def get_func(t,l,c,ratio):
+    return fun = lambda x: e**(-1*t*x/(2*l))*math.cos(t*math.sqrt(1/(l*c)-(x/(2*l))**2)) - ratio
 
-start = 0
-end = math.sqrt(4*l/c)
+def get_range(l,c):
+    return 0, math.sqrt(4*l/c)
